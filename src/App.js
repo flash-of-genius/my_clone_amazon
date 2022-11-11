f84e794a42dc13af9bc9325ec82cd22b4e1a2a36
flash-of-genius <amhdaar@gmail.com>
2022-11-11 16:44:34 +0100

    src\App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import './App.css';
 function App() {
   return (
     <div>
       <h1>Lets build an AMAZON Clone with React js</h1>
+      <h2>Hello World!</h2>
+
+      {/* we need React-ROUTER */}
+
+      {/* localhost.com/ */}
+      {/* localhost.com/checkout */}
+      {/* localhost.com/login */}
     </div>
   );
 }
