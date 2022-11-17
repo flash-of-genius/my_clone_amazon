6d4ed4fce9618c37e39527b06c660bee781b6ee4
flash-of-genius <amhdaar@gmail.com>
2022-11-17 16:44:43 +0100

    React Router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,42 @@
+/* eslint-disable no-lone-blocks */
+/* eslint-disable no-unused-vars */
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 function App() {
   return (
-    <div>
-      <h1>Lets build an AMAZON Clone with React js</h1>
-      <h2>Hello World!</h2>
-
-      {/* we need React-ROUTER */}
-
-      {/* localhost.com/ */}
-      {/* localhost.com/checkout */}
-      {/* localhost.com/login */}
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/checkout">
+            <h1>Checkout</h1>
+          </Route>
+          <Route path="/login">
+            <h1>Login Page</h1>
+          </Route>
+          {/* This is the default route, / */}
+          <Route path="/">
+            <h1>HOME PAGE!!</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
+}
+
+{
+  /* we need React-ROUTER */
+}
+
+{
+  /* localhost.com/ */
+}
+{
+  /* localhost.com/checkout */
+}
+{
+  /* localhost.com/login */
 }
 
 export default App;
